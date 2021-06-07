6864ffc207743d411af877161d6ee84ecdf276c3
carlosdizx <carlodiaz@umariana.edu.co>
2021-06-07 18:23:21 -0500

    Cargamos bootstrap en el proyecto, ademas de que creamos un navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="navbar navbar-dark bg-dark">
+    <router-link to="/" class="navbar-brand">
+      Formulario
+    </router-link>
+    <div class="d-flex">
+  <div class="btn btn-dark" to="/">
+    Tareas
+  </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
