53274129510bbf084b23938cb9d95f5e8f8eda54
carlosdizx <carlodiaz@umariana.edu.co>
2021-06-07 19:12:29 -0500

    Se agregaron los tipos de formularios, ademas de que se realizo una validacion

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
-  <form>
+  <form @submit.prevent="procesar">
     <input
         type="text"
         class="form-control my-2"
         placeholder="Ingrese nommbre"
-        v-model="tarea.nombre"
+        v-model.trim="tarea.nombre"
     />
     <div class="form-check form-check-inline">
       <input class="form-check-input" type="checkbox" id="check-1"
@@ -16,6 +16,26 @@
              v-model="tarea.categorias" value="NodeJS"/>
       <label class="form-check-label" for="check-2">NodeJS</label>
     </div>
+    <div class="mt-2">
+      <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id="radio-1" value="Urgente"
+               v-model="tarea.estado">
+        <label class="form-check-label" for="radio-1">Urgente</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id="radio-2" value="Relax"
+               v-model="tarea.estado">
+        <label class="form-check-label" for="radio-2">Relax</label>
+      </div>
+    </div>
+    <div class="mt-2">
+    <input class="form-control" type="number"
+    v-model.number="tarea.numero"/>
+    </div>
+    <button
+        class="btn btn-dark btn-block mt-2" type="submit"
+        :disabled="correcto"
+    >Procesar</button>
   </form>
   <hr>
   <p>{{ tarea }}</p>
@@ -31,9 +51,27 @@ export default {
     return {
       tarea:{
         nombre: '',
-        categorias: []
+        categorias: [],
+        estado :'',
+        numero: 0
       }
     }
   },
+  methods: {
+    procesar(){
+      console.log(this.tarea)
+      this.tarea = {
+        nombre: '',
+        categorias: [],
+        estado :'',
+        numero: 0
+      }
+    }
+  }
+  ,computed:{
+    correcto(){
+      return this.tarea.nombre.trim()==='' ? true : false
+    }
+  }
 }
 </script>
